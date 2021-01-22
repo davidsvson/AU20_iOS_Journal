6d8b735dfd0f7b49cc9bb9e00f84edada77240ef
David Svensson <davidsvson@gmail.com>
2021-01-22 09:45:21 +0100

    Initial Commit

---
+++ AU20_Journal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AU20_Journal
+//
+//  Created by David Svensson on 2021-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
